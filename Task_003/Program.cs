49b70ba149ee14d4f9de168c7cf9a1ddd8197713
Andrey <andrejsatykov@yandex.ru>
2022-12-10 21:26:00 +0300

    The difference between the maximum and minimum array elements Task 003

---
+++ Task_003/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+Console.Clear();
+
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int b = 0; b < numbers.Length; b++)
+{
+    if (numbers[b] > max)
+        {
+            max = numbers[b];
+        }
+    if (numbers[b] < min)
+        {
+            min = numbers[b];
+        }
+}
+double result = max - min;
+double itog = Math.Round(result, 2);
+
+Console.WriteLine($"Всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {itog}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+    }
+}
+
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+       Console.Write(numbers[i] + "; ");
+    }
+    
+    Console.Write("]");
+    Console.WriteLine();
+}
